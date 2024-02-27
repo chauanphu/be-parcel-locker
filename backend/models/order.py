f8e38c5fba8b2f22a3d8cfa8720c87eb749a93bd
Nhung55555 <itdsiu22167@student.hcmiu.edu.vn>
2024-02-27 19:37:01 +0700

    Update order.py

--- backend/models/order.py
+++ backend/models/order.py
@@ -2,19 +2,19 @@ from datetime import date
 from sqlalchemy import Column, String, VARCHAR, Date
 from database.__init__ import Base
 
-# class Order(Base):
-#     """
-#     Represents an order.
-#     """
+class Order(Base):
+    """
+    Represents an order.
+    """
 
-#     __tablename__ = 'order here!'
+    __tablename__ = 'order here!'
 
-#     package: Column(String) 
-#     sender: Column(VARCHAR(20))
-#     recipient: Column(VARCHAR(20))
-#     sending_locker: Column(String)
-#     receiving_locker: Column(String)
-#     sending_date: Column(Date)
-#     receiving_date: Column(Date)
+    package = Column(String,primary_key=True, index=True) 
+    sender = Column(VARCHAR(20), nullable=False)
+    recipient = Column(VARCHAR(20), nullable=False)
+    sending_locker = Column(String, nullable=False)
+    receiving_locker= Column(String, nullable=False)
+    sending_date = Column(Date)
+    receiving_date = Column(Date)
 
-# print("Order model created successfully.")
+print("Order model created successfully.")
