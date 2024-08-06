29d2df7c872946ef20dc938b5597f44ab6cb4ff5
QUAN <miqua2308@gmail.com>
2024-08-06 18:00:48 +0700

    add endpoint

--- backend/routers/location.py
+++ backend/routers/location.py
@@ -1,29 +1,29 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from typing import List
 from datetime import datetime
-from fastapi import APIRouter, Depends, HTTPException, Query
-from pydantic import BaseModel
-from sqlalchemy import func
-from database.session import get_db
-from models.locker import Locker, Cell
-from models.order import Order
-from sqlalchemy.orm import Session
-from typing import Any, Dict, List
-from pydantic import BaseModel
-from auth.utils import get_current_user
-from uuid import UUID
-from enum import Enum
-
-
 
 router = APIRouter(
     prefix="/location",
-    tags=["location"],
-    dependencies=[Depends(get_current_user)]
+    tags=["location"]
 )
 
+class GPSData(BaseModel):
+    latitude: float
+    longitude: float
+    timestamp: datetime
+    altitude: float = None
+    speed: float = None
+    accuracy: float = None
 
+# Temporary list to store location data
+location_data_list: List[GPSData] = []
 
-@router.get("/coordinate")
-async def get_locate():
-    return {
-        "Message": "Here is the coordinate"
-    }
+@router.post("/", response_model=GPSData)
+def receive_location_data(data: GPSData):
+    location_data_list.append(data)
+    return data
+
+@router.get("/", response_model=List[GPSData])
+def get_location_data():
+    return location_data_list
