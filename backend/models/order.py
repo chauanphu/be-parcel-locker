996874fdc58c3ab68dd76b11fdb7d86faaca54bf
QUAN <miqua2308@gmail.com>
2024-08-19 20:37:23 +0700

    ..

--- backend/models/order.py
+++ backend/models/order.py
@@ -25,7 +25,7 @@ class Order(Base):
     sending_date = Column(Date)
     receiving_date = Column(Date)
     
-    order_status = Column(Enum('Completed', 'Canceled','Ongoing','Delayed','Expired', name='order_status'), nullable=False,default='Ongoing')
+    order_status = Column(Enum( 'Completed', 'Canceled', 'Ongoing', 'Delayed' , 'Expired', name='order_status'), nullable=False,default='Ongoing')
     warnings = Column(Boolean, nullable=False, default=False)
     
     parcel = relationship('Parcel', backref= 'order',lazy=True, uselist=False)
