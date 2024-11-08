8e8a253d94287c81401d408b39faf6cee441950c
Phu Chau <chauanphu@gmail.com>
2024-11-08 16:30:32 +0700

    feat: add get current user information

--- backend/auth/router.py
+++ backend/auth/router.py
@@ -4,9 +4,12 @@ from fastapi.security import OAuth2PasswordRequestForm
 from starlette import status
 from sqlalchemy.orm import Session
 from starlette import status
-from auth.utils import authenticate_user, create_access_token
+from auth.utils import authenticate_user, create_access_token, get_current_user
 from database.session import get_db
 from pydantic import BaseModel
+
+from models.account import Account
+from models.role import Role
 
 router = APIRouter(
     prefix='/auth',
@@ -17,6 +20,10 @@ router = APIRouter(
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+class UserInfo(BaseModel):
+    username: str
+    role: str
 
 @router.post("/token", response_model=Token)
 async def login_for_access_token(
@@ -34,8 +41,14 @@ async def login_for_access_token(
         "token_type": 'bearer'
     }
 
-# @router.get("/login", status_code=status.HTTP_200_OK)
-# async def user(current_user: User = Depends(get_current_user)):
-#     if current_user is None:
-#         raise HTTPException(status_code=401, detail='Authentication Failed')
-#     return {"User": user}
+@router.get("/me", response_model=UserInfo)
+async def read_users_me(
+        current_user: Account = Depends(get_current_user),
+        db: Session = Depends(get_db)):
+    role = db.query(Role).filter(Role.role_id == current_user.role).first()
+    if not role:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Could not validate user',
+                            )
+    return UserInfo(username=current_user.username, role=role.name)
+
--- backend/models/role.py
+++ backend/models/role.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from database.__init__ import Base
 
 
@@ -11,4 +12,4 @@ class Role(Base):
     
     role_id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
-    
+    accounts = relationship("Account", back_populates="role")
