8352c37d2e4b3991adf5f08e895ffcd43a154b12
Phu Chau <chauanphu@gmail.com>
2024-11-08 09:30:03 +0700

    add firmware router

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -7,13 +7,10 @@ from .parcel import router as parcel_router
 from .location import router as location_router
 
 from .account import router as account_router
-from .account import public_router as a_public_router
-# from .account import shipper_router as shipper_router
 
 from .profile import router as profile_router
 from .recipient import router as recipent_router
-from .shipper import router as shipper_router
-
+from .file import router as file_router
 api_router = APIRouter(prefix="/api/v1")
 
 
@@ -27,10 +24,10 @@ api_router.include_router(parcel_router)
 api_router.include_router(location_router)
 
 api_router.include_router(account_router)
-api_router.include_router(a_public_router)
 
 api_router.include_router(profile_router)
 api_router.include_router(recipent_router)
+api_router.include_router(file_router)
 # api_router.include_router(shipper_router)
 
 
--- backend/routers/account.py
+++ backend/routers/account.py
@@ -6,16 +6,11 @@ from database.session import get_db
 from models.profile import Profile
 from models.account import Account
 from sqlalchemy.orm import Session
-from auth.utils import get_current_user,bcrypt_context,check_admin
+from auth.utils import get_current_user, bcrypt_context, check_admin
 from starlette import status
 from enum import Enum
-from jose import JWTError, jwt
 from decouple import config
 import random
-
-SECRET_KEY= config("SECRET_KEY")
-ALGORITHM = config("algorithm", default="HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = 10
 
 class StatusEnum(str, Enum):
     Active = 'Active'
@@ -28,23 +23,16 @@ router = APIRouter(
     dependencies=[Depends(get_current_user)]
 )
 
-public_router = APIRouter(
-    prefix="/account",
-    tags=["account"]
-)
-
 class Address(BaseModel):
     address_number: str
     street: str
     ward: str
     district: str
 
-
 class CreateUserRequest(BaseModel):
     email: EmailStr
     username: str
     password: str = Field(..., min_length=6)
-
 
 class CreateAdminRequest(BaseModel):
     email: str
@@ -61,14 +49,14 @@ class UserResponse(BaseModel):
     status: StatusEnum
     Date_created: datetime
     role: int
+
 class RegisterUserRequest(BaseModel):
     username: str
     email: EmailStr
     password: str = Field(..., min_length=6)
     confirm_password: str
 
-
-
+pending_users = {}
 
 def create_access_code(data: dict, expires_delta: timedelta = None):
     if expires_delta:
@@ -78,7 +66,6 @@ def create_access_code(data: dict, expires_delta: timedelta = None):
     
     access_code = random.randint(100000, 999999)
     
-    # Store the access code and expiration time
     pending_users[data["email"]] = {
         "username": data["username"],
         "password": data["password"],
@@ -88,91 +75,13 @@ def create_access_code(data: dict, expires_delta: timedelta = None):
     
     return access_code
 
-
-pending_users = {} # For pending users
-
-
-#This doesn't need right now 
-
-# @public_router.post('/Register_by_code', status_code=status.HTTP_201_CREATED)
-# async def register_user(register_user_request: RegisterUserRequest, db: Session = Depends(get_db)):
-#     if register_user_request.password != register_user_request.confirm_password:
-#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-#                             detail='Confirm password not like password')
-    
-#     user = authenticate_user(register_user_request.username, register_user_request.password, db)
-#     if user:
-#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-#                             detail='Email already exists')
-    
-#     if register_user_request.username == Account.username:
-#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-#                             detail='User already exists')
-        
-#     token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-#     access_code = create_access_code(
-#         data={"email": register_user_request.email, "username": register_user_request.username, "password": bcrypt_context.hash(register_user_request.password)}, 
-#         expires_delta=token_expires
-#     )
-    
-#     message = MessageSchema(
-#         subject="Email Confirmation",
-#         recipients=[register_user_request.email],
-#         body=f"Your confirmation code is: {access_code}",
-#         subtype="html"
-#     )
-    
-#     fm = FastMail(conf)
-#     await fm.send_message(message)
-    
-#     return {"message": "Please check your email for the confirmation code"}
-
-
-
-
-# @public_router.post("/confirm_code", status_code=status.HTTP_201_CREATED)
-# async def confirm_email(code: int, email: str, db: Session = Depends(get_db)):
-#     user_data = pending_users.get(email)
-#     if user_data is None:
-#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid or expired code")
-    
-#     if user_data["access_code"] != code:
-#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid code")
-    
-#     if datetime.utcnow() > user_data["expires_at"]:
-#         pending_users.pop(email)
-#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Expired code")
-    
-#     new_user = Account(email=email, username=user_data["username"], password=user_data["password"])
-#     db.add(new_user)
-#     db.commit()
-#     db.refresh(new_user)
-    
-#     new_profile = Profile(
-#         user_id=new_user.user_id,
-#         name = 'null',
-#         gender = 'Male',
-#         age = 0,
-#         phone = 0,
-#         address = 'null'
-#         )
-#     db.add(new_profile)
-#     db.commit()
-#     pending_users.pop(email)
-    
-#     return {"message": "Email confirmed and user registered successfully"}
-
-
 @router.get("/accounts", response_model=Dict[str, Any], dependencies=[Depends(check_admin)])
 def get_paging_accounts(
     db: Session = Depends(get_db),
     page: int = Query(1, ge=1),
     per_page: int = Query(10, ge=1)
 ):
-    # Total number of accounts
     total_accounts = db.query(Account).count()
-
-    # Fetch paginated list of accounts
     accounts = (
         db.query(Account)
         .offset((page - 1) * per_page)
@@ -180,7 +89,6 @@ def get_paging_accounts(
         .all()
     )
 
-    # Format the response
     account_responses = [
         {
             "user_id": account.user_id,
@@ -211,7 +119,6 @@ async def create_account_user(account: CreateUserRequest, db: Session = Depends(
     check_user_username = db.query(Account).filter(Account.username == account.username).first()
     if check_user_username is not None:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="The username already exists")
-   
     
     new_account = Account(email=account.email, username=account.username, password=account.password)
     db.add(new_account)
@@ -236,8 +143,6 @@ async def delete_account_user(user_id: int, db: Session = Depends(get_db)):
     db.delete(profile)
     db.commit()
     
-    
     return {
-        "Message": "Account deleted sucessfully"
+        "Message": "Account deleted successfully"
     }
-    
