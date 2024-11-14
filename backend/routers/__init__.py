1ddf8db5b41ea64bbb657a1d54f56fbc1f46dbb0
Phu Chau <chauanphu@gmail.com>
2024-11-14 10:15:27 +0700

    opt: remove unused location

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -4,7 +4,6 @@ from auth.router import router as auth_router
 from .locker import router as locker_router
 from .order import router as order_router
 from .parcel import router as parcel_router
-from .location import router as location_router
 from .account import router as account_router
 from .shipper import router as shipper_router
 
@@ -16,7 +15,6 @@ api_router.include_router(auth_router)
 api_router.include_router(locker_router)
 api_router.include_router(order_router)
 api_router.include_router(parcel_router)
-api_router.include_router(location_router)
 api_router.include_router(account_router)
 api_router.include_router(shipper_router)
 
